02034bf70db6851aec3def3d3bc5aae279da6d8b
Christopher Spraggett <cspraggett@gmail.com>
2020-01-15 13:18:54 -0500

    refactored play and created client

---
+++ client.js
@@ -0,0 +1,16 @@
+const net = require("net");
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: "172.46.2.204",
+    port: 50541
+  });
+  conn.setEncoding("utf8");
+
+  conn.on("data", data => {
+    console.log(data);
+  });
+  return conn;
+};
+
+module.exports = { connect };

f342c761219bac9067c4519ff2f279a1ed6aeb3d
Christopher Spraggett <cspraggett@gmail.com>
2020-01-15 14:16:10 -0500

    added movement

--- input.js
+++ input.js
@@ -1,11 +1,28 @@
+let connection;
+
 let handleUserInput = key => {
   if (key === "\u0003") {
     process.exit();
   }
+  switch (key.toLowerCase()) {
+    case "w":
+      connection.write("Move: up");
+      break;
+    case "a":
+      connection.write("Move: left");
+      break;
+    case "s":
+      connection.write("Move: down");
+      break;
+    case "d":
+      connection.write("Move: right");
+      break;
+  }
 };
 
-let setupInput = () => {
+let setupInput = conn => {
   const stdin = process.stdin;
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
--- play.js
+++ play.js
@@ -3,6 +3,5 @@ const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 
-setupInput();
-
-connect();
+// connect();
+setupInput(connect());
